2b72f7f782b5d26d26eaf3412a69036d541decf8
Adnan Unal <a.unal677@gmail.com>
2023-01-07 16:08:13 +0100

    Add useTrafficEvents hook

---
+++ src/Hooks/useTrafficEvents.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { sfBayApi } from "../Api/sfBayApi";
+import { ITrafficEvent } from "../Api/types";
+
+export const useTrafficEvents = () => {
+  const [trafficEvents, setTrafficEvents] = useState<ITrafficEvent[]>([]);
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const res = await sfBayApi.getTrafficEvents();
+      setTrafficEvents(res);
+    } catch (err) {
+      setTrafficEvents([]);
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { trafficEvents, error, loading, refetch: getData };
+};
